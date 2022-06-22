671cb508cf4690812dbfa4d9295ee20679a21277
Kevin Roney <ktroney.pdx@gmail.com>
2022-06-22 14:18:46 -0700

    wrote test function to test user signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const mockUser = {
+  email: 'test@test.com',
+  password: 'testpassword',
+};
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('creates a new user', async () => {
+    const res = await request(app)
+      .post('/users')
+      .send(mockUser);
+    expect(res.body.user.email).toEqual('test@test.com');
   });
   afterAll(() => {
     pool.end();
